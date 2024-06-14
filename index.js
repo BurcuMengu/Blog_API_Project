30c5f9b617d0ea8576e8162050617cdba7710ca0
BurcuMengu <burcumengu@gmail.com>
2024-06-14 17:03:20 +0300

    push -u origin main

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
-import axios from "axios";
 
 const app = express();
 const port = 4000;
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 
 
